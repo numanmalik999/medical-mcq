a5d20c4fd311fb4f6fb46400b7e6dfc1bc3fa36a
[dyad] <numan_malik999@yahoo.com>
2025-11-08 23:51:54 +0500

    [dyad] Forced redeployment of failed Stripe functions - wrote 2 file(s)

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -1,4 +1,5 @@
 // @ts-ignore
+// v2 - Forcing redeployment
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 // @ts-ignore
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';

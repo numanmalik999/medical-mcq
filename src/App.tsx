a4990d5e771280b958053e94eed1892b9ad16067
[dyad] <numan_malik999@yahoo.com>
2026-01-01 00:59:58 +0300

    [dyad] Added automatic scroll-to-top on page navigation. - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop"; // Import ScrollToTop
 import LandingPage from "./pages/LandingPage";
 import DashboardRedirect from "./pages/DashboardRedirect";
 import NotFound from "./pages/NotFound";
@@ -65,6 +66,7 @@ const AppContent = () => {
 
   return (
     <>
+      <ScrollToTop /> {/* Add ScrollToTop here */}
       <Header />
       <div className="flex flex-col min-h-screen">
         <div className="flex-grow">

482487b5ca1c7536217de78a9323a7ce57149d59
[dyad] <numan_malik999@yahoo.com>
2026-02-07 00:58:14 +0300

    [dyad] Auto-collapsing sidebar on study pages - wrote 1 file(s)

--- src/components/UserLayout.tsx
+++ src/components/UserLayout.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import UserSidebar from './UserSidebar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 const UserLayout = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    // Automatically collapse the sidebar when in "Study Mode" routes
+    const isStudyMode = location.pathname === '/quiz' || location.pathname === '/user/take-test';
+    
+    if (isStudyMode) {
+      setIsCollapsed(true);
+    }
+  }, [location.pathname]);
 
   return (
     <div className="flex min-h-screen bg-background text-foreground pt-16">
-      <UserSidebar isCollapsed={isCollapsed} onToggleCollapse={() => setIsCollapsed(!isCollapsed)} />
+      <UserSidebar 
+        isCollapsed={isCollapsed} 
+        onToggleCollapse={() => setIsCollapsed(!isCollapsed)} 
+      />
       <div className="flex-1">
         <div className="p-4 sm:p-6 lg:p-8">
           <Outlet />
